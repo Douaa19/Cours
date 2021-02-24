9ca5f642fe72045047585f25ee4bca8e2437760c
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-02-24 10:52:20 +0100

    CorectionExercice5

--- Workshop_PHP/exercice4/main.php
+++ Workshop_PHP/exercice4/main.php
@@ -1,17 +1,17 @@
+<?php
+
+    
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Text Typing animation</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Bonjour, je m'appelle Douaa Larif index</h1>
-    <a href="main.php?nom=LARIF&prenom=Douaa">Send</a>
-
-
-
-</body>
+    <h1>Bonjour, je m'appelle <?php echo $_GET['nom']; ?> <?php echo $_GET['prenom']; ?> main</h1>
 </html>

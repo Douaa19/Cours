7f55e351e3fc970cf1ea26aba8f29cd9f16a5b48
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-04-07 11:59:43 +0100

    Vérification_système

--- Mini_chat/includes/singup-inc.php
+++ Mini_chat/includes/singup-inc.php
@@ -6,6 +6,7 @@ if(!include_once('../database.php')){
     die();
 }
 
+
 // Récupérer les données avec la méthode POST.
 
     // La vérification des données.
@@ -21,14 +22,21 @@ if(!include_once('../database.php')){
         } else {
             // Si les inputs ne sont pas vides
             // Validation email
-            $insert = 'INSERT INTO utilisateurs(nom, pseudo, email, mdp) VALUES ("' . $nom . '" , "' . $pseudo . '" , "' . $email . '" , "' . $mdp . '")';
-            $result = mysqli_query($link, $insert);
-            if ($result) {
-                header('Location: ../index.php');
+            $select = "SELECT * FROM utilisateurs WHERE nom = '$nom' AND pseudo = '$pseudo' AND email = '$email' AND mdp = '$mdp'";
+            $res = mysqli_query($link, $select);
+            if (mysqli_num_rows($res) == 1) {
+                header('Location: ../sing-up.php?This_informations_repated!');
                 die();
             } else {
-                echo "Error: " . $insert . " " . mysqli_error($link);
-                die();
+                $insert = 'INSERT INTO utilisateurs(nom, pseudo, email, mdp) VALUES ("' . $nom . '" , "' . $pseudo . '" , "' . $email . '" , "' . $mdp . '")';
+                $result = mysqli_query($link, $insert);
+                if ($result) {
+                    header('Location: ../index.php');
+                    die();
+                } else {
+                    echo "Error: " . $insert . " " . mysqli_error($link);
+                    die();
+                } 
             }
         }
     }

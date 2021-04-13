70083b2b6b960a1fcc7625e54b2a25a24cd8125b
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-04-13 10:57:11 +0000

    Project4

--- js-projects/projet-4/app.js
+++ js-projects/projet-4/app.js
@@ -1,11 +1,23 @@
-const input = document.querySelector(".addItems-input");
-const add = document.querySelector(".addItems-submitt");
-const clear = document.querySelector(".displayItems-clear");
+var addToDoButton = document.querySelector('.addItems-submit');
+var inputField = document.querySelector('.addItems-input');
+var toDoContainer = document.querySelector('.displayItems-container');
 
-Form.addEventListener('add', function (e) {
-    e.preventDefault();
-    input.innerHTML += 
+addToDoButton.addEventListener('click', function(ele) {
+    ele.preventDefault();
+    var paragraph = document.createElement('p');
+    let addItems = document.querySelector(".addItems");
+    let addItemsText = document.createTextNode(inputField.value);
+    addItems.appendChild(addItemsText);
+    inputField.value = "";
+    let displayItemsClear = document.querySelector(".displayItems-clear");
+    let tqediaList = document.querySelector(".tqedia-list");
+    displayItemsClear.addEventListener('click', function() {
+        tqediaList.removeChild(addItems);
+    })
+
+
 })
+
 
 
 
